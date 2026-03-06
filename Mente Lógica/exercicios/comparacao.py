2fa78f8a792aa957ec30857836a018dfe2065d31
Matheusedc <matheuseddc@gmail.com>
2026-03-06 11:12:23 -0300

    Update comparacao.py

--- Mente Lógica/exercicios/comparacao.py
+++ Mente Lógica/exercicios/comparacao.py
@@ -87,8 +87,25 @@ Crie um programa que verifica se um ano é bissexto.
 ● Masnãoébissexto se for divisível por 100, exceto se for divisível por 400.
 '''
 
-entrada = int(2020)
+entrada = int(2021)
 
-divisao = entrada / 4
+divisao = entrada % 4
 
-divisao100 = entrada / 100
+divisao100 = entrada % 100
+
+divisao400 = entrada % 400
+
+if (divisao == 0 and divisao100 != 0) or (divisao400 ==0 ):
+    
+    print("O ano é bissexto")
+else:
+    print("O ano não é bissexto")
+    
+
+'''
+    5. Simulador de Caixa Eletrônico
+Crie um programa que simula um caixa eletrônico. O usuário deve informar o valor do saque
+(apenas valores inteiros) e o programa deve informar quantas cédulas de cada valor serão
+fornecidas.
+● Considere cédulas de R$100, R$50, R$20, R$10, R$5 e R$2.
+'''
